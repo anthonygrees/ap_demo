f348e0d58205d4842982d41a55384df6829d935e
Anthony Rees <anthony_rees01@hotmail.com>
2020-07-09 12:34:04 +1000

    Update windows2012_acsc.rb

--- recipes/windows2012_acsc.rb
+++ recipes/windows2012_acsc.rb
@@ -12,14 +12,14 @@ end
 
   powershell_script 'Create config.rb' do
     code <<-EOH
-    $nodeName = "Win2012-correct-{0}" -f (-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_}))
+    $nodeName = "Win2012-acsc-ism-{0}" -f (-join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_}))
   
     $clientrb = @"
 chef_server_url 'https://#{node['environment']['automate_url']}/organizations/#{node['environment']['chef_org']}'
 validation_key 'C:\\Users\\Administrator\\AppData\\Local\\Temp\\kitchen\\cookbooks\\ap_demo\\recipes\\validator.pem'
 node_name '{0}'
 policy_group 'development'
-policy_name 'base_windows2012_correct'
+policy_name 'base_windows2012_acsc'
 ssl_verify_mode :verify_none
 chef_license 'accept'
 "@ -f $nodeName
