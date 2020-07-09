a63f26b5bbb444329eefd1864be3d779fca84713
Anthony Rees <anthony_rees01@hotmail.com>
2020-07-09 12:54:07 +1000

    Update policy_push.ps1

--- policyfiles/policy_push.ps1
+++ policyfiles/policy_push.ps1
@@ -4,10 +4,13 @@ chef install base_windows2012_detect.rb
 chef install base_windows2012_correct.rb
 chef install base_windows2016_detect.rb
 chef install base_windows2016_correct.rb
+chef install base_windows2012_acsc.rb
 chef push development base_linux_correct.rb
 chef push development base_linux_detect.rb
 chef push development base_windows2012_detect.rb
 chef push development base_windows2012_correct.rb
 chef push development base_windows2016_detect.rb
 chef push development base_windows2016_correct.rb
+chef push development base_windows2012_acsc.rb
+
 copy C:\chef-repo\.chef\reesyorg.pem C:\chef-repo\cookbooks\ap_demo\recipes\validator.pem
