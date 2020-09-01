8ada4de74cbd73aa5c6a3fbaece5f8d8066271a5
Anthony Rees <anthony_rees01@hotmail.com>
2020-09-01 15:18:07 +1000

    Update base_linux_detect.rb

--- policyfiles/base_linux_detect.rb
+++ policyfiles/base_linux_detect.rb
@@ -10,7 +10,7 @@ name 'base_linux_detect'
 default_source :chef_server, "https://#{ENV['AUTOMATE_HOSTNAME']}/organizations/#{ENV['CHEF_ORG']}"
 
 # Specify a custom source for a cookbook:
-cookbook 'chef-client', '~> 10.2.2' ## Stage 1 - Base
+cookbook 'chef-client', '~> 12.2.0' ## Stage 1 - Base
 cookbook 'audit_agr', '~> 2.2.4' ## Stage 2 - Detect
 
 # run_list: chef-client will run these recipes in the order specified.
